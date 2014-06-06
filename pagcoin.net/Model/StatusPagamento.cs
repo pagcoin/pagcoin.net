4469f0d209c364b119e38592518c8e8955cf03f3
unknown <amirton@pagcoin.com>
2014-06-06 11:16:23 -0300

    Removendo valores depreciados do enum

--- pagcoin.net/Model/StatusPagamento.cs
+++ pagcoin.net/Model/StatusPagamento.cs
@@ -5,11 +5,8 @@
         Iniciada = 1,
         Recebida = 2,
         Confirmada = 3,
-        VendedorInformado = 4,
-        ValorTransferidoColdStorage = 5,
         Rejeitada = 6,
         ParcialmenteRecebida = 7,
-        Timeout = 8,
-        ValorTransferidoVendedor = 9
+        Timeout = 8
     }
 }
